756139a55ab50dbd9eb2ad7a9140bfa137a805e7
SrimannarayanP <78963283+SrimannarayanP@users.noreply.github.com>
2025-10-26 02:14:55 +0530

    Added UserSerializer for CustomUser model
    Implemented UserSerializer for CustomUser model to handle user data serialization and deserialization.

---
+++ events_app/backend/api/serializers.py
@@ -0,0 +1,28 @@
+# serializers.py
+
+
+# Here, we are going to use something called as a serializer. Basically, serializers help in converting Python objects into JSON objects & vice-versa. We'll be using an
+# ORM that will help us in converting the database tables into Python objects. So, to convert these Python objects into JSON objects that can be sent over HTTP, we use
+# serializers.
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+
+        model = CustomUser
+
+        fields = ['id', 'username', 'email', 'password']
+
+        extra_kwargs = {'password' : {'write_only' : True}} # This ensures that when we're creating a new user, we accept a password but we don't want to return the
+                                                            # password when we return info. about the user
+
+    # Really, what's happening here is that the serializer will look at this model, look at all the fields on that model & the ones we've defined, validate them & if the
+    # data is valid, it'll pass it as validated_data in the function
+    def create(self, validated_data):
+
+        user = CustomUser.objects.create_user(**validated_data)
+
+        return user
