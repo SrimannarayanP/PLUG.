33f52e19ca13d9d578a87b139222c8154bfc6a3d
SrimannarayanP <78963283+SrimannarayanP@users.noreply.github.com>
2025-10-26 02:09:47 +0530

    Added NotFound component for 404 error page

---
+++ events_app/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,21 @@
+// NotFound.jsx
+
+
+function NotFound() {
+
+    return (
+
+        <div>
+
+            <h1>Not found</h1>
+
+            <p>The page you're looking for doesn't exist!</p>
+
+        </div>
+
+    )
+    
+}
+
+
+export default NotFound;
