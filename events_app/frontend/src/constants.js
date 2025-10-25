fceaba08817c7ea0e332d04c31d705e6c4f24471
SrimannarayanP <78963283+SrimannarayanP@users.noreply.github.com>
2025-10-26 02:11:40 +0530

    Added constants for authentication tokens
    This file defines constants for access and refresh tokens used in authentication.

---
+++ events_app/frontend/src/constants.js
@@ -0,0 +1,8 @@
+// constants.js
+
+// Basically what this does is creates keys for the tokens used in authentication. These tokens are going to be stored in our local storage for now, therefore, these keys
+// will help us access them easily.
+
+
+export const ACCESS_TOKEN = 'access'
+export const REFRESH_TOKEN = 'refresh'
