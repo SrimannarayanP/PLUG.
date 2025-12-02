f8e96b8a917c8bb07e450e792b45f8242ab7f89e
SrimannarayanP <78963283+SrimannarayanP@users.noreply.github.com>
2025-12-02 23:05:07 +0530

    Added new routes for StudentDashboard and HostScanner

--- events_app/frontend/src/App.jsx
+++ events_app/frontend/src/App.jsx
@@ -8,6 +8,10 @@ import NotFound from './pages/NotFound';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import SuccessfulLogin from './pages/SuccessfulLogin';
+import StudentDashboard from './pages/StudentDashboard'
+import HostScanner from './pages/HostScanner';
+import CreateEvent from './pages/CreateEvent';
+import HostDashboard from './pages/HostDashboard'
 
 
 function Logout() {
@@ -25,32 +29,52 @@ export default function App() {
         <BrowserRouter>
         
             <Routes>
-
-                <Route path = '/' element = {
-                    
-                    <ProtectedRoute>
-
-                        <LandingPage />
-                    
-                    </ProtectedRoute>
-                    
-                } />
                 <Route 
-                
+                    path = '/'
+                    element = {
+                        <LandingPage />
+                    }
+                />
+
+                <Route 
                     path = '/success_login'
                     element = {
-
                         <ProtectedRoute>
-
                             <SuccessfulLogin />
-
                         </ProtectedRoute>
-    
                     }    
-
                 />
+
+                <Route 
+                    path = '/student_dashboard'
+                    element = {
+                        <ProtectedRoute>
+                            <StudentDashboard />
+                        </ProtectedRoute>
+                    }    
+                />
+
+                <Route 
+                    path = '/host/dashboard'
+                    element = {
+                        <ProtectedRoute>
+                            <HostDashboard />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path = '/host/create-event'
+                    element = {
+                        <ProtectedRoute>
+                            <CreateEvent />
+                        </ProtectedRoute>
+                    }
+                />
+                {/* <Route path = '/student_dashboard' element = {<StudentDashboard />} />  */}
                 <Route path = '/login' element = {<LoginSignup />} />
                 <Route path = '/logout' element = {<Logout />} />
+                <Route path = '/scan' element = {<HostScanner />} />
                 {/* If the user tries to go to any other route apart from the ones defined it'll show error 404 - Not found page */}
                 <Route path = '*' element = {<NotFound />} />
                 
