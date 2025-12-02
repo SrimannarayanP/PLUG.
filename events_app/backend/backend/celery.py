3d77a47e4b081d082c5b5e8bf97ac1d1396dfd68
SrimannarayanP <78963283+SrimannarayanP@users.noreply.github.com>
2025-12-02 22:50:00 +0530

    Set up Celery integration with Django
    Initialize Celery with Django settings and autodiscover tasks.

---
+++ events_app/backend/backend/celery.py
@@ -0,0 +1,19 @@
+# celery.py
+
+
+
+import os
+
+from celery import Celery
+
+
+# Setting default settings to Django project settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+app = Celery('backend')
+
+# This tells Celery: "Look at Django's settings.py file." namespace = 'CELERY' means it will only read variables that start with 'CELERY_' like 'CELERY_BROKER_URL'
+app.config_from_object('django.conf:settings', namespace = 'CELERY')
+
+# It tells Celery to look through every installed app & automatically find a file named 'tasks.py'. If this wasn't there, we'd have to manually import every task here.
+app.autodiscover_tasks()
