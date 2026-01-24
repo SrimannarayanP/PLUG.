445c231e384c70c6bbbe4dd014e08650363e10d3
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-01-24 19:46:31 +0530

    Added Vite configuration for React & Tailwind CSS
    Configured Vite with React, Tailwind CSS & SSL plugins. Set up build options & server proxy for API requests.

---
+++ events_app/frontend/src/vite.config.js
@@ -0,0 +1,51 @@
+import {defineConfig} from 'vite'
+
+import react from '@vitejs/plugin-react'
+
+import tailwindcss from '@tailwindcss/vite'
+
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+
+export default defineConfig({
+
+    plugins: [
+        react(), 
+        tailwindcss(),
+        basicSsl(),
+    ],
+    build : {
+        minify : 'terser', // Remove all unnecessary chars like spaces, comments, newlines from code
+        rollupOptions : {
+            manualChunks(id) {
+                // Put all libs in separate 'vendor' chunks.
+                if (id.includes('node_modules')) {
+                    // Split huge libs into their own chunks.
+                    if (id.includes('lucide-react')) {
+
+                        return 'icons'
+
+                    }
+
+                    if (id.includes('framer-motion')) {
+
+                        return 'animation'
+
+                    }
+
+                    return vendor
+                }
+            }
+        }
+    },
+    server : {
+        host : true,
+        proxy : {
+            '/api' : {
+                target : 'http://127.0.0.1:8000',
+                changeOrigin : true,
+            },
+        }
+    }
+
+})
