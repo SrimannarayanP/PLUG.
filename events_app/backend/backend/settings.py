8a899464339b1dc82a4927aaf9c482e33341d815
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-02-14 00:51:22 +0530

    Refactored settings.py to use environment variables
    Updated settings to use environment variables for sensitive data & configuration. Adjusted token lifetimes & added Cloudinary storage settings.

--- events_app/backend/backend/settings.py
+++ events_app/backend/backend/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 
-import os, pymysql
+import os, pymysql, dj_database_url
 
 
 pymysql.install_as_MySQLdb()
@@ -21,12 +21,12 @@ load_dotenv(BASE_DIR / '.env') # This looks for a .env file at the root director
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!lycoi9s#l!(!qje5soswbq=18v-bbp3e-kbf!6&fnrfng7z&t'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["*"] # Allows any different host to host our Django app
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',') # Allows any different host to host our Django app
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : (
@@ -40,35 +40,44 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-
-    'ACCESS_TOKEN_LIFETIME' : timedelta(minutes = 30),
+    'ACCESS_TOKEN_LIFETIME' : timedelta(minutes = 45),
     'REFRESH_TOKEN_LIFETIME' : timedelta(days = 1),
-
 }
 
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
     'api',
     'rest_framework',
     'corsheaders',
 ]
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME' : os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY' : os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET' : os.environ.get('CLOUDINARY_API_SECRET')
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -76,7 +85,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -95,14 +104,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'events',
-        'USER' : 'Sriman',
-        'PASSWORD' : 'SpitterDune#891*',
-        'HOST' : 'localhost',
-        'PORT' : '3306',
-    }
+    'default': dj_database_url.config(
+        default = os.getenv('DATABASE_URL'),
+        conn_max_age = 600
+    )
 }
 
 
@@ -141,13 +146,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = os.getenv('DEBUG') == 'True'
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
 CORS_ALLOW_CREDENTIALS = True
 
 AUTH_USER_MODEL = 'api.CustomUser'
@@ -162,9 +170,10 @@ if not TICKET_SIGNING_KEY:
     raise ValueError("Missing TICKET_SIGNING_KEY in .env file")
 
 # CELERY SETTINGS
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 CELERY_TIMEZONE = 'UTC'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # For development, emails will be printed to console
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
