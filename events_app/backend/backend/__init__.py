17c365fdc4e7889f1bda15e2cceb85185d4f0cd5
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-02-14 10:23:29 +0530

    Updated __init__.py

--- events_app/backend/backend/__init__.py
+++ events_app/backend/backend/__init__.py
@@ -1,8 +1,5 @@
 from .celery import app as celery_app
-import pymysql
 
 
-pymysql.version_info = (2, 2, 1, 'final', 0)
-pymysql.install_as_MySQLdb()
 # This ensures that Celery starts when Django starts
 __all__ = ('celery_app',)
