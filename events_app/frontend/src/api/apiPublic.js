46688c93ab4d7ea90e28472ef2e978b8b230da8d
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-02-14 12:55:46 +0530

    Updated apiPublic.js

--- events_app/frontend/src/api/apiPublic.js
+++ events_app/frontend/src/api/apiPublic.js
@@ -5,7 +5,9 @@
 import axios from 'axios'
 
 
-const apiPublic = axios.create()
+const apiPublic = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'https://plug-production.up.railway.app'
+})
 
 
 export default apiPublic
