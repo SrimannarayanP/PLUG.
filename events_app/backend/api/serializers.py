d26ea5b412b24c77ce1adbd701d6cd36199acf8a
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-02-27 23:44:49 +0530

    Enhancd serializers with new fields & logic

--- events_app/backend/api/serializers.py
+++ events_app/backend/api/serializers.py
@@ -19,16 +19,14 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-from datetime import datetime
-
 from .models import Category, CustomUser, Event, EventDocument, OrganisationProfile, Registration, SchoolCollege, StudentProfile
 from .tasks import send_verification_email
-from .utils import  generate_qr_code_base64, generate_ticket_token, generate_otp
+from .utils import  generate_qr_code_base64, generate_ticket_token, generate_otp, track_unlisted_school_college_request
 
 User = get_user_model()
 
 
-# Auth & Token Serializers
+# --- Auth & Token Serializers ---
 # This view is for getting the access & refresh tokens for login. It's custom because it uses email instead of username for verification.
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
@@ -146,16 +144,18 @@ class SchoolCollegeSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = SchoolCollege
-        fields = ['id', 'name', 'city', 'state']
+        fields = ['id', 'name', 'campus', 'city', 'state']
 
 
 # --- Profile Serializers ---
 class OrganisationProfileSerializer(serializers.ModelSerializer):
+    
+    school_college = SchoolCollegeSerializer(read_only = True)
 
     class Meta:
 
         model = OrganisationProfile
-        fields = ['name' ,'phone_number']
+        fields = ['name' ,'phone_number', 'school_college']
 
 
 class StudentProfileSerializer(serializers.ModelSerializer):
@@ -165,7 +165,7 @@ class StudentProfileSerializer(serializers.ModelSerializer):
     class Meta:
         
         model = StudentProfile
-        fields = ['date_of_birth', 'phone_number', 'school_college', 'student_id_number'] # joined_organisation
+        fields = ['date_of_birth', 'phone_number', 'school_college', 'student_id_number', 'unlisted_school_college_data'] # joined_organisation
 
 
 class StudentOrgRelationSerializer(serializers.ModelSerializer):
@@ -186,6 +186,7 @@ class UserSerializer(serializers.ModelSerializer):
     phone_number = serializers.CharField(write_only = True, required = False)
     date_of_birth = serializers.DateField(write_only = True, required = False)
     student_id_number = serializers.CharField(write_only = True, required = False)
+
     school_college_id = serializers.PrimaryKeyRelatedField(
         queryset = SchoolCollege.objects.all(),
         source = 'school_college',
@@ -193,6 +194,8 @@ class UserSerializer(serializers.ModelSerializer):
         required = False,
         allow_null = True
     )
+    unlisted_school_college_data = serializers.JSONField(write_only = True, required = False, allow_null = True)
+
     profile = serializers.SerializerMethodField()
 
     class Meta:
@@ -200,7 +203,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = [
             'id', 'email', 'password', 'first_name', 'last_name', 'role', 'organisation_name', 'phone_number', 'date_of_birth', 'student_id_number', 'school_college_id', 
-            'profile', 'is_email_verified'
+            'unlisted_school_college_data', 'profile', 'is_email_verified'
         ]
         extra_kwargs = {
             'password' : {'write_only' : True}, # This ensures that when we're creating a new user, we accept a password but we don't want to return the
@@ -219,6 +222,7 @@ class UserSerializer(serializers.ModelSerializer):
         date_of_birth = validated_data.pop('date_of_birth', None)
         student_id_number = validated_data.pop('student_id_number', None)
         school_college = validated_data.pop('school_college', None)
+        unlisted_school_college_data = validated_data.pop('unlisted_school_college_data', None)
         role = validated_data.get('role', CustomUser.Role.STUDENT)
 
         user = User.objects.create_user(**validated_data)
@@ -233,14 +237,30 @@ class UserSerializer(serializers.ModelSerializer):
             OrganisationProfile.objects.create(
                 user = user, 
                 name = org_name, 
-                phone_number = phone_number
+                phone_number = phone_number,
+                school_college = school_college
             )
         else:
             # Default to student
-            StudentProfile.objects.create(user = user, phone_number = phone_number, date_of_birth = date_of_birth, student_id_number = student_id_number, school_college = school_college)
+            StudentProfile.objects.create(
+                user = user,
+                phone_number = phone_number,
+                date_of_birth = date_of_birth,
+                student_id_number = student_id_number,
+                school_college = school_college,
+                unlisted_school_college_data = unlisted_school_college_data if not school_college else {}
+            )
+
+            if unlisted_school_college_data and not school_college:
+                track_unlisted_school_college_request(
+                    name = unlisted_school_college_data.get('name'),
+                    campus = unlisted_school_college_data.get('campus'),
+                    city = unlisted_school_college_data.get('city'),
+                    state = unlisted_school_college_data.get('state')
+                )
 
         # We use on_commit to ensure the user exists in DB before Celery tries to find them.
-        send_verification_email.delay(str(user.id), otp)
+        transaction.on_commit(lambda: send_verification_email.delay(str(user.id), otp))
 
         return user
     
@@ -258,12 +278,15 @@ class UserSerializer(serializers.ModelSerializer):
     
     @transaction.atomic
     def update(self, instance, validated_data):
-
         phone_number = validated_data.pop('phone_number', None)
         org_name = validated_data.pop('organisation_name', None)
-        school_college = validated_data.pop('school_college', None)
         date_of_birth = validated_data.pop('date_of_birth', None)
-        
+        student_id_number = validated_data.pop('student_id_number', None) 
+
+        update_school_college = 'school_college' in validated_data
+        school_college = validated_data.pop('school_college', None)
+        unlisted_school_college_data = validated_data.pop('unlisted_school_college_data', None)
+
         # Updated standard user fields like first_name, last_name, etc.
         for attr, value in validated_data.items():
             if attr == 'password':
@@ -277,25 +300,52 @@ class UserSerializer(serializers.ModelSerializer):
         if hasattr(instance, 'organisation_profile'):
             profile = instance.organisation_profile
 
-            if phone_number:
+            if phone_number is not None:
                 profile.phone_number = phone_number
 
-            if org_name: 
+            if org_name is not None:
                 profile.name = org_name
+
+            if update_school_college:
+                profile.school_college = school_college
 
             profile.save()
 
         if hasattr(instance, 'student_profile'):
             profile = instance.student_profile
 
-            if phone_number:
+            if phone_number is not None:
                 profile.phone_number = phone_number
 
-            if date_of_birth:
+            if date_of_birth is not None:
                 profile.date_of_birth = date_of_birth
 
-            if school_college:
+            if student_id_number is not None:
+                profile.student_id_number = student_id_number
+
+            if update_school_college:
                 profile.school_college = school_college
+
+                if school_college is not None:
+                    profile.unlisted_school_college_data = {}
+                elif unlisted_school_college_data:
+                    profile.unlisted_school_college_data = unlisted_school_college_data
+
+                    track_unlisted_school_college_request(
+                        name = unlisted_school_college_data.get('name'),
+                        campus = unlisted_school_college_data.get('campus'),
+                        city = unlisted_school_college_data.get('city'),
+                        state = unlisted_school_college_data.get('state')
+                    )
+            elif unlisted_school_college_data is not None:
+                profile.unlisted_school_college_data = unlisted_school_college_data
+
+                track_unlisted_school_college_request(
+                    name = unlisted_school_college_data.get('name'),
+                    campus = unlisted_school_college_data.get('campus'),
+                    city = unlisted_school_college_data.get('city'),
+                    state = unlisted_school_college_data.get('state')
+                )
 
             profile.save()
 
@@ -338,7 +388,16 @@ class EventSerializer(serializers.ModelSerializer):
     start_date_formatted = serializers.DateTimeField(source = 'start_date', format = r"%B %d, %Y, %I:%M %p", read_only = True)
     registration_deadline = serializers.DateTimeField(required = False, allow_null = True)
 
-    is_registration_open = serializers.BooleanField(read_only = True)
+    is_registration_open = serializers.BooleanField(read_only = True)    
+    is_internal_event = serializers.SerializerMethodField()
+
+    restricted_to_school_college_ids = serializers.PrimaryKeyRelatedField(
+        queryset = SchoolCollege.objects.all(),
+        source = 'restricted_to_schools_colleges',
+        required = False,
+        allow_null = True,
+        many = True
+    )
 
     class Meta:
 
@@ -347,8 +406,12 @@ class EventSerializer(serializers.ModelSerializer):
             'id', 'name', 'description', 'start_date', 'start_date_formatted', 'end_date', 'location_type', 'physical_location', 'google_maps_link',
             'virtual_location', 'register_link', 'registration_deadline', 'is_native', 'is_featured', 'poster', 'is_paid_event', 'payment_qr_image',
             'ticket_price', 'max_tickets_per_user', 'organisation', 'categories', 'category_ids', 'is_registration_open', 'age_restriction_cutoff', 'collect_phone',
-            'collect_college_school', 'collect_student_id', 'documents', 'uploaded_documents'
+            'collect_college_school', 'collect_student_id', 'documents', 'uploaded_documents', 'restricted_to_school_college_ids', 'is_internal_event'
         ]
+
+    def get_is_internal_event(self, obj):
+
+        return obj.restricted_to_schools_colleges.exists()
 
     def validate(self, attrs):
         """Before creating/updating, we create a dummy instance & run .clean() on it to check if there's bad data in the request sent."""
@@ -467,6 +530,7 @@ class TicketAttendeeSerializer(serializers.Serializer):
 
     school_college_id = serializers.IntegerField(required = False, allow_null = True)
     school_college_name = serializers.CharField(required = False, allow_blank = True)
+    school_college_campus = serializers.CharField(required = False, allow_blank = True)
     school_college_city = serializers.CharField(required = False, allow_blank = True)
     school_college_state = serializers.CharField(required = False, allow_blank = True)
 
@@ -489,9 +553,24 @@ class BulkRegistrationSerializer(serializers.Serializer):
 
             raise serializers.ValidationError({'event_id' : "Event not found."})
         
+        internal_colleges = event.restricted_to_schools_colleges.all()
+        is_internal_event = internal_colleges.exists()
+
+        if is_internal_event:
+            if not user.is_authenticated or not hasattr(user, 'student_profile'):
+
+                raise serializers.ValidationError("You must be a verified student of the restricted schools/colleges to book tickets.")
+            
+            if user.student_profile.school_college not in internal_colleges:
+                allowed_schools_colleges = ", ".join([f"{sc.name} - {sc.campus}" if sc.campus else sc.name for sc in internal_colleges])
+
+                raise serializers.ValidationError(f"This event is strictly internal to: {allowed_schools_colleges}.")
+        
         if len(attendees) > event.max_tickets_per_user:
 
             raise serializers.ValidationError(f"You can only book a maximum of {event.max_tickets_per_user} tickets.")
+        
+        internal_college_ids = [sc.id for sc in internal_colleges] if is_internal_event else []
         
         for index, attendee in enumerate(attendees):
             is_buyer = (index == 0)
@@ -504,6 +583,14 @@ class BulkRegistrationSerializer(serializers.Serializer):
                 if not attendee.get('last_name'):
 
                     raise serializers.ValidationError({f"attendees[{index}].last_name" : "Guest's last name is required."})
+                
+            if is_internal_event and not is_buyer:
+                guest_college_id = attendee.get('school_college_id')
+
+                if not guest_college_id or guest_college_id not in internal_college_ids:
+                    allowed_schools_colleges = ", ".join([f"{sc.name} - {sc.campus}" if sc.campus else sc.name for sc in internal_colleges])
+
+                    raise serializers.ValidationError({f"attendees[{index}].school_college" : f"All guests must explicitly belong to one of: {allowed_schools_colleges}"})
 
             # Phone Check
             if event.collect_phone:
@@ -531,30 +618,16 @@ class BulkRegistrationSerializer(serializers.Serializer):
                     raise serializers.ValidationError({f"attendees[{index}].student_id_number" : "Student ID number is required."})
                     
             # School/College Check
-            if event.collect_college_school:
+            if event.collect_college_school and not is_internal_event:
                 if attendee.get('school_college_id'):
                     pass
                 elif attendee.get('school_college_name'):
-                    raw_name = attendee.get('school_college_name').strip()
-                    raw_city = attendee.get('school_college_city').strip()
-                    raw_state = attendee.get('school_college_state').strip()
+                    raw_city = attendee.get('school_college_city', '').strip()
+                    raw_state = attendee.get('school_college_state', '').strip()
 
                     if not raw_city or not raw_state:
                         
                         raise serializers.ValidationError({f"attendees[{index}].school_college" : "To add a new college, you MUST provide its City & State."})
-                    
-                    college_obj, created = SchoolCollege.objects.get_or_create(
-                        name__iexact = raw_name,
-                        city__iexact = raw_city,
-                        defaults = {
-                            'name' : raw_name.title(),
-                            'city' : raw_city.title(),
-                            'state' : raw_state.title()
-                        }
-                    )
-
-                    # After inserting into DB, inject that ID into this data
-                    attendee['school_college_id'] = college_obj.id
                 elif is_buyer and user.student_profile.school_college:
                     pass
                 else:
@@ -605,8 +678,27 @@ class AttendeeListSerializer(serializers.ModelSerializer):
         return obj.guest_data.get('phone_number') or obj.student.phone_number or 'N/A'
     
     def get_school_college(Self, obj):
+        guest_sc = obj.guest_data.get('school_college_name')
 
-        return obj.guest_data.get('school_college_name') or (str(obj.student.school_college) if obj.student.school_college else 'N/A')
+        if guest_sc:
+
+            return guest_sc
+        
+        if obj.student.school_college:
+            sc = obj.student.school_college
+
+            return f"{sc.name} - {sc.campus} ({sc.city})" if sc.campus else f"{sc.name} ({sc.city})"
+        
+        unlisted = obj.student.unlisted_school_college_data
+
+        if unlisted and isinstance(unlisted, dict) and unlisted.get('name'):
+            name = unlisted.get('name')
+            campus = unlisted.get('campus')
+            city = unlisted.get('city')
+
+            return f"{name} - {campus} ({city}) [Unverified]" if campus else f"{name} ({city}) [Unverified]"
+        
+        return 'N/A'
     
     def get_student_id_number(self, obj):
 
