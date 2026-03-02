1fd62e9acc1e432d183115a7199096f798a0cb23
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-03-02 19:55:57 +0530

    Configured Celery beat schedule for ticket release
    Added a beat schedule to release stale tickets every 2 minutes.

--- events_app/backend/backend/celery.py
+++ events_app/backend/backend/celery.py
@@ -4,6 +4,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 
 # Setting default settings to Django project settings
@@ -16,3 +17,10 @@ app.config_from_object('django.conf:settings', namespace = 'CELERY')
 
 # It tells Celery to look through every installed app & automatically find a file named 'tasks.py'. If this wasn't there, we'd have to manually import every task here.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'release-stale-tickets-every-15-mins' : {
+        'task' : 'api.tasks.release_abandoned_tickets',
+        'schedule' : crontab(minute = '*/2') # Run check every 2 minutes.
+    }
+}
