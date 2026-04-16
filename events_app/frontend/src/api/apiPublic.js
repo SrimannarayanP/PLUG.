1e627ce9d8bc3e8b6dc171d2f922826886ec9742
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-04-16 09:31:08 +0530

    Enhanced axios instance with headers & timeout
    Added headers & timeout to axios instance.

--- events_app/frontend/src/api/apiPublic.js
+++ events_app/frontend/src/api/apiPublic.js
@@ -1,12 +1,16 @@
 // apiPublic.js
 
 
-
 import axios from 'axios'
 
 
 const apiPublic = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+    baseURL : import.meta.env.VITE_API_URL,
+    headers : {
+        'Content-Type' : 'application/json',
+        'Accept' : 'application/json',
+    },
+    timeout : 10000
 })
 
 
