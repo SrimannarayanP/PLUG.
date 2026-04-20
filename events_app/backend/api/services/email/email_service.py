3ea200c949d9352f8da9cd179b248313a887b527
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-04-20 20:10:24 +0530

    Added email_service.py to send emails via Brevo
    Implemented email sending functionality using Brevo API.

---
+++ events_app/backend/api/services/email/email_service.py
@@ -0,0 +1,41 @@
+# email_service.py
+
+
+from django.conf import settings
+
+import requests
+
+
+BREVO_URL = 'https://api.brevo.com/v3/smtp/email'
+
+
+def send_email(to_email, to_name, subject, html_content, attachment = None):
+    payload = {
+        'sender' : {
+            'name' : settings.BREVO_FROM_NAME,
+            'email' : settings.BREVO_FROM_EMAIL,
+        },
+        'to' : [
+            {
+                'email' : to_email,
+                'name' : to_name or 'User'
+            }
+        ],
+        'subject' : subject,
+        'htmlContent' : html_content
+    }
+
+    if attachment:
+        payload['attachment'] = attachment
+
+    headers = {
+        'accept' : 'application/json',
+        'api-key' : settings.BREVO_API_KEY,
+        'content-type' : 'application/json'
+    }
+
+    response = requests.post(BREVO_URL, json = payload, headers = headers, timeout = 10)
+
+    response.raise_for_status()
+
+    return response.json()
